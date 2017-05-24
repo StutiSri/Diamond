2f7469539399a326ae6ae09ca7028be93677dd76
StutiSri <stutisrivastv@gmail.com>
2017-05-25 00:05:17 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int num = Integer.parseInt(args[0]);
+        for(int i=0;i<num;i++){
+            for(int j=num-i-1;j>0;j--){
+                System.out.print(" ");
+            }
+            for(int j=0;j<(i*2)+1;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        for(int i=num-1;i>=0;i--){
+            for(int j=num-i-1;j>0;j--){
+                System.out.print(" ");
+            }
+            for(int j=0;j<(i*2)+1;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
